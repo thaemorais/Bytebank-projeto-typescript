e42ffbbdfc0d42e0359f6f1685e2e6cae1ea84fc
thaemorais <thaelenml15@gmail.com>
2024-04-17 21:09:07 -0300

    Configuração inicial do localStorage

--- dist/js/types/Conta.js
+++ dist/js/types/Conta.js
@@ -1,5 +1,11 @@
 import { TipoTransacao } from "./TipoTransacao.js";
 let saldo = 500;
+const transacoes = JSON.parse(localStorage.getItem("transacoes"), (key, value) => {
+    if (key === "data") {
+        return new Date(value);
+    }
+    return value;
+}) || [];
 function debitar(valor) {
     if (valor <= 0) {
         throw new Error("O valor a ser debitado deve ser maior que zero!");
@@ -33,7 +39,9 @@ const Conta = {
         else {
             throw new Error("Tipo de transação inválida.");
         }
+        transacoes.push(novaTransacao);
         console.log(novaTransacao);
+        localStorage.setItem("transacoes", JSON.stringify(transacoes));
     },
 };
 export default Conta;
--- src/types/Conta.ts
+++ src/types/Conta.ts
@@ -2,6 +2,16 @@ import { Transacao } from "./Transacao.js";
 import { TipoTransacao } from "./TipoTransacao.js";
 
 let saldo: number = 500;
+const transacoes: Transacao[] =
+	JSON.parse(
+		localStorage.getItem("transacoes"),
+		(key: string, value: string) => {
+			if (key === "data") {
+				return new Date(value);
+			}
+			return value;
+		}
+	) || [];
 
 function debitar(valor: number): void {
 	if (valor <= 0) {
@@ -42,7 +52,9 @@ const Conta = {
 		} else {
 			throw new Error("Tipo de transação inválida.");
 		}
+		transacoes.push(novaTransacao);
 		console.log(novaTransacao);
+		localStorage.setItem("transacoes", JSON.stringify(transacoes));
 	},
 };
 
